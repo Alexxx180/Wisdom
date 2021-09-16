80d6cdf36fb42d1479ae27b9700f8253255017f0
Alexxx180 <alexx120702@gmail.com>
2021-09-16 23:09:37 +0300

    Alpha 1.22 | Add OOXML table logic

--- Wisdom/Writers/ResultRenderer.cs
+++ Wisdom/Writers/ResultRenderer.cs
@@ -1,33 +1,78 @@
-﻿using DocumentFormat.OpenXml.Packaging;
+﻿using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Wordprocessing;
-using System.Collections.Generic;
-//using System.Diagnostics;
 
 namespace Wisdom.Writers
 {
     public static class ResultRenderer
     {
+        private static Paragraph FastText(string text)
+        {
+            return new Paragraph(new ParagraphProperties(
+                new RunFonts
+                {
+                    Ascii = "Times New Roman",
+                    HighAnsi = "Times New Roman",
+                    EastAsia = "Times New Roman",
+                    ComplexScript = "Times New Roman"
+                },
+                new FontSize { Val = new StringValue("28") }
+                ),
+                new Run(new RunProperties(
+                new RunFonts
+                {
+                    Ascii = "Times New Roman",
+                    HighAnsi = "Times New Roman",
+                    EastAsia = "Times New Roman",
+                    ComplexScript = "Times New Roman"
+                },
+                new FontSize { Val = new StringValue("28") }),
+                new Text(text)));
+        }
+        private static TableCellWidth CellWidth(string value)
+        {
+            TableCellWidth width = new TableCellWidth
+            {
+                Width = new StringValue(value),
+                Type = TableWidthUnitValues.Pct
+            };
+            return width;
+        }
+        private static Table DefaultTable()
+        {
+            TableWidth tabWidth = new TableWidth
+            {
+                Width = new StringValue("100%"),
+                Type = TableWidthUnitValues.Pct
+            };
+            return new Table(new TableProperties(tabWidth));
+        }
+
+        private static Table FillTable(Table table, string width1, string width2, params string[] texts)
+        {
+            for (byte i = 0; i < texts.Length; i++)
+            {
+                TableCell cell0 = new TableCell(FastText("\n"), new TableCellProperties(CellWidth(width1)));
+                TableCell cell = new TableCell(FastText(texts[i]), new TableCellProperties(CellWidth(width2)));
+                TableRow row = new TableRow(cell0, cell);
+                table.AddChild(row);
+            }
+            return table;
+        }
+        //Method 5, Using OOXML can save as docx
         public static void WriteDoc(System.Windows.Documents.FlowDocument doc, string filename)
         {
-            //using (FileStream fs = new FileStream(filename, FileMode.OpenOrCreate, FileAccess.ReadWrite))
-            //{
-            //    System.Windows.Documents.TextRange textRange = new System.Windows.Documents.TextRange(doc.ContentStart, doc.ContentEnd);
-            //    textRange.Save(fs, DataFormats.Rtf);
-            //}
-
-            //Method 5, Using OOXML can save as docx
-            using (WordprocessingDocument package = WordprocessingDocument.Open(@"C:\Users\Администратор\Desktop\Работы\Курсовая\Shit.docx", true)) //, WordprocessingDocumentType.Document
+            using (WordprocessingDocument package = WordprocessingDocument.Create(
+                @"C:\Users\Администратор\Desktop\Работы\Курсовая\Shit.docx", WordprocessingDocumentType.Document))
             {
-                IEnumerable<Table> list = package.MainDocumentPart.Document.Descendants<Table>();
-                foreach (Table tab in list)
-                {
-                    //Trace.WriteLine(tab.ChildElements.Count);
-                    TableRow row = tab.ChildElements.GetItem(2) as TableRow;
-                    //Trace.WriteLine(row.ChildElements.Count);
-                    TableCell cell = row.ChildElements.GetItem(1) as TableCell;
-                    //Trace.WriteLine(cell.InnerText);
-                    cell.AppendChild(new Paragraph(new Run(new Text("LOOOL"))));
-                }
+                MainDocumentPart mainPart = package.AddMainDocumentPart();
+                new Document(new Body()).Save(mainPart);
+                //row.ChildElements.GetItem(1)
+                Body body = mainPart.Document.Body;
+                mainPart.Document.Save();
+
+                Table tab = FillTable(DefaultTable(), "50%", "50%", "123123");
+                body.Append(tab);
                 package.Save();
                 package.Close();
             }
