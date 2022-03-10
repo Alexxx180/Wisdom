c9edbff80ca62c344c95c0fbf7eddcd62842b2a0
Alexxx180 <alexx120702@gmail.com>
2022-03-11 00:02:18 +0300

    Beta 1.085 | Stable pre-release

--- Wisdom/obj/Release/netcoreapp3.1/Controls/Tables/MetaData/MetaElementAdditor.g.i.cs
+++ Wisdom/obj/Release/netcoreapp3.1/Controls/Tables/MetaData/MetaElementAdditor.g.i.cs
@@ -53,7 +53,7 @@ namespace Wisdom.Controls.Tables.MetaData {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Wisdom;component/controls/tables/metadata/metaelementadditor.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Wisdom;V1.0.0.0;component/controls/tables/metadata/metaelementadditor.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\..\..\Controls\Tables\MetaData\MetaElementAdditor.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
