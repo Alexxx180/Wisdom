a44053b1058e11b0d1d132341c9587ae34e56371
Tatarintsev Aleksandr <67857746+Alexxx180@users.noreply.github.com>
2022-08-31 18:28:54 +0300

    Source - string Hybrid Node

--- Tables/Source.cs
+++ Tables/Source.cs
@@ -2,25 +2,8 @@
 
 namespace ControlMaterials.Tables
 {
-    public class Source
+    public class Source : NameNode<string>
     {
-        public Source()
-        {
-
-        }
-
-        public Source(
-            string name,
-            List<string>
-            descriptions
-            )
-        {
-            Name = name;
-            Descriptions = descriptions;
-        }
-
-        public string Name { get; set; }
-
-        public List<string> Descriptions { get; set; }
+        public Source() { }
     }
 }
