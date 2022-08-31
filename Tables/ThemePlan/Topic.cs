1c9c2b8b8638d264a737fc349bd8350b07978913
Tatarintsev Aleksandr <67857746+Alexxx180@users.noreply.github.com>
2022-08-31 15:14:51 +0300

    Topic properties update - notifiers

--- Tables/ThemePlan/Topic.cs
+++ Tables/ThemePlan/Topic.cs
@@ -1,11 +1,56 @@
-﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Collections.Generic;
 
 namespace ControlMaterials.Tables.ThemePlan
 {
     public class Topic
     {
-        public string Name { get; set; }
-        public string Hours { get; set; }
-        public List<Theme> Themes { get; set; }
+        public Topic()
+        {
+            Themes = new ObservableCollection<Theme>();
+        }
+        
+        private string _name;
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                _name = value;
+                OnPropertyChanged();
+            }
+        }
+        
+        private ushort _hours;
+        public ushort Hours
+        {
+            get => _hours;
+            set
+            {
+                _hours = value;
+                OnPropertyChanged();
+            }
+        }
+        
+        public ObservableCollection<Theme> Themes { get; }
+        
+        #region INotifyPropertyChanged Members
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Raises this object's PropertyChanged event.
+        /// </summary>
+        /// <param name="propertyName">The property that has a new value.</param>
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                PropertyChangedEventArgs e = new PropertyChangedEventArgs(propertyName);
+                handler(this, e);
+            }
+        }
+        #endregion
     }
 }
