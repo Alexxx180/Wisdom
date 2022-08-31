9ab606904e83458e79b2d5b257cdd397b3aa2be4
Tatarintsev Aleksandr <67857746+Alexxx180@users.noreply.github.com>
2022-08-31 15:33:47 +0300

    Theme update: notify property changed

--- Tables/ThemePlan/Theme.cs
+++ Tables/ThemePlan/Theme.cs
@@ -1,13 +1,80 @@
-﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Collections.Generic;
 
 namespace ControlMaterials.Tables.ThemePlan
 {
     public class Theme
     {
+        public Theme()
+        {
+            Works = new ObservableCollection<Work>();
+        }
+        
         public string Name { get; set; }
-        public string Hours { get; set; }
-        public string Competetions { get; set; }
-        public string Level { get; set; }
-        public List<Work> Works { get; set; }
+        
+        private ushort _hours;
+        public ushort Hours
+        {
+            get => _hours;
+            set
+            {
+                _hours = value;
+                OnPropertyChanged();
+            }
+        }
+        
+        private ObservableCollection<Competetion> _gCompetetions;
+        public ObservableCollection<Competetion> GCompetetions
+        {
+            get => _gCompetetions;
+            set
+            {
+                _gCompetetions = value;
+                OnPropertyChanged();
+            }
+        }
+        
+        private ObservableCollection<Competetion> _pCompetetions;
+        public ObservableCollection<Competetion> PCompetetions
+        {
+            get => _pCompetetions;
+            set
+            {
+                _pCompetetions = value;
+                OnPropertyChanged();
+            }
+        }
+        
+        private ushort _level;
+        public ushort Level
+        {
+            get => _level;
+            set
+            {
+                _level = value;
+                OnPropertyChanged();
+            }
+        }
+        
+        public ObservableCollection<Work> Works { get; }
+        
+        #region INotifyPropertyChanged Members
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Raises this object's PropertyChanged event.
+        /// </summary>
+        /// <param name="propertyName">The property that has a new value.</param>
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                PropertyChangedEventArgs e = new PropertyChangedEventArgs(propertyName);
+                handler(this, e);
+            }
+        }
+        #endregion
     }
 }
