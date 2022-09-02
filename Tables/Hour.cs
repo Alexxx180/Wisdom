914a6354917c5ca9fc648a71761886157b7ebd76
Tatarintsev Aleksandr <67857746+Alexxx180@users.noreply.github.com>
2022-09-02 17:26:00 +0300

    Updated Hour - inherit from name label

--- Tables/Hour.cs
+++ Tables/Hour.cs
@@ -3,25 +3,14 @@ using System.Runtime.CompilerServices;
 
 namespace ControlMaterials.Tables
 {
-    public class Hour
+    public class Hour : NameLabel
     {
         public Hour(string name, ushort count)
         {
             Name = name;
             Count = count;
         }
-
-        private string _name;
-        public string Name
-        {
-            get => _name;
-            set
-            {
-                _name = value;
-                OnPropertyChanged();
-            }
-        }
-
+        
         private ushort _count;
         public ushort Count
         {
@@ -32,23 +21,5 @@ namespace ControlMaterials.Tables
                 OnPropertyChanged();
             }
         }
-
-        #region INotifyPropertyChanged Members
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        /// <summary>
-        /// Raises this object's PropertyChanged event.
-        /// </summary>
-        /// <param name="propertyName">The property that has a new value.</param>
-        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            PropertyChangedEventHandler handler = PropertyChanged;
-            if (handler != null)
-            {
-                PropertyChangedEventArgs e = new PropertyChangedEventArgs(propertyName);
-                handler(this, e);
-            }
-        }
-        #endregion
     }
 }
