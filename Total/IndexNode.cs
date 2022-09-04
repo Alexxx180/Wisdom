e8d230701425f1853923e8b2217ba2f519a5da3f
Alexxx180 <alexx120702@gmail.com>
2022-09-04 13:24:00 +0300

    Node add-remove methods

--- Total/IndexNode.cs
+++ Total/IndexNode.cs
@@ -8,7 +8,17 @@ namespace ControlMaterials.Total
         {
             Items = new ObservableCollection<T>();
         }
-        
+
+        public void Add(T item)
+        {
+            Items.Add(item);
+        }
+
+        public bool Remove(T item)
+        {
+            return Items.Remove(item);
+        }
+
         public ObservableCollection<T> Items { get; }
     }
 }
--- Total/NameNode.cs
+++ Total/NameNode.cs
@@ -8,6 +8,16 @@ namespace ControlMaterials.Total
         {
             Items = new ObservableCollection<T>();
         }
+
+        public void Add(T item)
+        {
+            Items.Add(item);
+        }
+
+        public bool Remove(T item)
+        {
+            return Items.Remove(item);
+        }
         
         public ObservableCollection<T> Items { get; }
     }
