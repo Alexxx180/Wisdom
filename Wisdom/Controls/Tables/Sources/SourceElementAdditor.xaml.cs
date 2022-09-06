4816c192d962a1f5284e04306d02e1ef853bbd78
Tatarintsev Aleksandr <67857746+Alexxx180@users.noreply.github.com>
2022-09-06 16:33:15 +0300

    Refactoring Source Element Additor: new item

--- Wisdom/Controls/Tables/Sources/SourceElementAdditor.xaml.cs
+++ Wisdom/Controls/Tables/Sources/SourceElementAdditor.xaml.cs
@@ -1,94 +1,23 @@
-﻿using System.Windows;
+using ControlMaterials.Tables;
+using ControlMaterials.Total;
+using System.Windows;
 using System.Windows.Controls;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using Wisdom.Controls.Tables.Sources.SourceTypes;
+using Wisdom.Controls.Templates;
 
 namespace Wisdom.Controls.Tables.Sources
 {
     /// <summary>
     /// Special component to add new source
     /// </summary>
-    public partial class SourceElementAdditor : UserControl, INotifyPropertyChanged, IAutoIndexing
+    public partial class SourceElementAdditor : NewItem<IndexedLabel>
     {
-        public static readonly DependencyProperty
-            TypeProperty = DependencyProperty.Register(nameof(SourceType),
-                typeof(SourceType), typeof(SourceElementAdditor));
-
-        #region IAutoIndexing
-        private uint _no;
-        public uint No
-        {
-            get => _no;
-            set
-            {
-                _no = value;
-                OnPropertyChanged();
-                OnPropertyChanged(nameof(SourceHeader));
-            }
-        }
-
-        public void Index(uint no)
-        {
-            No = no;
-        }
-        #endregion
-
-        #region SourceAdditor Members
-        public SourceType SourceType
-        {
-            get => GetValue(TypeProperty) as SourceType;
-            set => SetValue(TypeProperty, value);
-        }
-
-        public string SourceHeader => $"{No}.";
-
-        public string _value;
-        public string Source
-        {
-            get => _value;
-            set
-            {
-                _value = value;
-                OnPropertyChanged();
-            }
-        }
-        #endregion
-
         public SourceElementAdditor()
         {
             InitializeComponent();
-            Index(1);
+            Data = new IndexedLabel();
         }
-
-        private void AddSource(object sender, RoutedEventArgs e)
-        {
-            SourceElement source = new SourceElement
-            {
-                No = No,
-                Source = Source,
-                //SourceType = SourceType
-            };
-            //SourceType.AddRecord(source);
-            Index(No + 1);
-        }
-
-        #region INotifyPropertyChanged Members
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        /// <summary>
-        /// Raises this object's PropertyChanged event.
-        /// </summary>
-        /// <param name="propertyName">The property that has a new value.</param>
-        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            PropertyChangedEventHandler handler = PropertyChanged;
-            if (handler != null)
-            {
-                PropertyChangedEventArgs e = new PropertyChangedEventArgs(propertyName);
-                handler(this, e);
-            }
-        }
-        #endregion
     }
 }
