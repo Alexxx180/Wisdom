7cc728dfb714e7cad4a6a22e6c93e5a52239e50f
Tatarintsev Aleksandr <67857746+Alexxx180@users.noreply.github.com>
2022-09-06 15:39:51 +0300

    Refactoring Competetion: Old Item template

--- Wisdom/Controls/Tables/Competetions/Competetion.xaml.cs
+++ Wisdom/Controls/Tables/Competetions/Competetion.xaml.cs
@@ -7,169 +7,16 @@ using System.Windows.Input;
 using System.Windows.Controls;
 using ControlMaterials.Tables;
 using ControlMaterials.Tables.ThemePlan;
-
 using static Wisdom.Customing.Converters;
 using CompetetionModel = ControlMaterials.Tables.Competetion;
 
 namespace Wisdom.Controls.Tables.Competetions
 {
     /// <summary>
-    /// General competetion related to speciality | discipline
+    /// General competetion related to speciality-discipline
     /// </summary>
-    public partial class Competetion : UserControl, INotifyPropertyChanged, IOptionableIndexing, IRawData<Competetion>, IExtendableItems, IWrapFields
+    public partial class Competetion : OldItem<CompetetionModel>, IExtendableItems, IWrapFields
     {
-        #region Dependency Properties
-        public static readonly DependencyProperty
-            DataProperty = DependencyProperty.Register(nameof(Data),
-                typeof(CompetetionModel), typeof(Competetion));
-
-        public static readonly DependencyProperty
-            RemoveProperty = DependencyProperty.Register(nameof(Remove),
-                typeof(ICommand), typeof(Competetion));
-
-        public ICommand Remove
-        {
-            get => GetValue(RemoveProperty) as ICommand;
-            set => SetValue(RemoveProperty, value);
-        }
-        
-        public CompetetionModel Data
-        {
-            get => GetValue(DataProperty) as CompetetionModel;
-            set => SetValue(DataProperty, value);
-        }
-        #endregion
-    
-    
-        #region IRawData Members
-        public Competetion Raw()
-        {
-            return new Competetion
-            {
-                Name = GeneralName,
-
-                //PrefixNo = GeneralHeader,
-                //Abilities = new List<Task> {
-                //    new Task("Умения", GeneralSkills),
-                //    new Task("Знания", GeneralKnowledge)
-                //}
-            };
-        }
-
-        public void SetElement(Competetion competetion)
-        {
-            //string prefixNo = Regex.Match(competetion.PrefixNo, @"\d+").Value;
-            //GeneralNo = prefixNo;
-
-            //GeneralName = competetion.Name;
-
-            //List<Task> data = competetion.Abilities;
-            //GeneralSkills = data[0].Hours;
-            //GeneralKnowledge = data[1].Hours;
-        }
-        #endregion
-
-        #region IOptionableIndexing Members
-        private AutoPanel _options;
-        public AutoPanel Options
-        {
-            get => _options;
-            set
-            {
-                _options = value;
-                OnPropertyChanged();
-            }
-        }
-        #endregion
-
-        #region IAutoIndexing Members
-        private uint _no;
-        public uint No
-        {
-            get => _no;
-            set
-            {
-                _no = value;
-                OnPropertyChanged();
-
-                if (Options is null ||
-                    Options.IsManual)
-                    return;
-
-                GeneralNo = value.ToGeneralNo();
-            }
-        }
-
-        public void Index(uint no)
-        {
-            No = no;
-        }
-        #endregion
-
-        #region GeneralCompetetion Members
-        public string Prefix => "ОК";
-        public string GeneralHeader => $"{Prefix} {GeneralNo}";
-
-        private string _generalNo;
-        public string GeneralNo
-        {
-            get => _generalNo;
-            set
-            {
-                if (value == "")
-                {
-                    return;
-                }
-
-                uint no = value.ParseHours();
-                _generalNo = no.ToGeneralNo();
-                
-
-                if (Options != null &&
-                    Options.IsManual)
-                {
-                    Index(no);
-                }
-
-                OnPropertyChanged();
-                OnPropertyChanged(nameof(GeneralHeader));
-            }
-        }
-
-        private string _generalName;
-        public string GeneralName
-        {
-            get => _generalName;
-            set
-            {
-                _generalName = value;
-                OnPropertyChanged();
-            }
-        }
-
-        private string _generalSkills;
-        public string GeneralSkills
-        {
-            get => _generalSkills;
-            set
-            {
-                _generalSkills = value;
-                OnPropertyChanged();
-            }
-        }
-
-        private string _generalKnowledge;
-        public string GeneralKnowledge
-        {
-            get => _generalKnowledge;
-            set
-            {
-                _generalKnowledge = value;
-                OnPropertyChanged();
-            }
-        }
-        #endregion
-
         #region IExtendableItems Members
         private bool _extended;
         public bool Extended
@@ -209,32 +56,8 @@ namespace Wisdom.Controls.Tables.Competetions
         public Competetion()
         {
             InitializeComponent();
-            Index(1);
             Extended = true;
             IsWrap = false;
         }
-
-        private void DropCompetetion(object sender, RoutedEventArgs e)
-        {
-            Options.DropRecord(this);
-        }
-
-        #region INotifyPropertyChanged Members
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        /// <summary>
-        /// Raises this object's PropertyChanged event.
-        /// </summary>
-        /// <param name="propertyName">The property that has a new value.</param>
-        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            PropertyChangedEventHandler handler = PropertyChanged;
-            if (handler != null)
-            {
-                PropertyChangedEventArgs e = new PropertyChangedEventArgs(propertyName);
-                handler(this, e);
-            }
-        }
-        #endregion
     }
 }
