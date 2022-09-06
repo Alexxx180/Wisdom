9598e937224c05668dc9b450138bb7bde1e138a2
Alexxx180 <alexx120702@gmail.com>
2022-09-06 08:11:25 +0300

    Command bindings review

--- Wisdom/obj/Release/net5.0-windows/Controls/Tables/EducationLevels/EducationLevelAdditor.g.i.cs
+++ Wisdom/obj/Release/net5.0-windows/Controls/Tables/EducationLevels/EducationLevelAdditor.g.i.cs
@@ -53,8 +53,7 @@ namespace Wisdom.Controls.Tables.EducationLevels {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Wisdom;V1.0.0.0;component/controls/tables/educationlevels/educationleveladditor." +
-                    "xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Wisdom;component/controls/tables/educationlevels/educationleveladditor.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\..\..\Controls\Tables\EducationLevels\EducationLevelAdditor.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
--- Wisdom/obj/Release/net5.0-windows/Controls/Tables/ThemePlan/Themes/Works/PlanWorkAdditor.g.i.cs
+++ Wisdom/obj/Release/net5.0-windows/Controls/Tables/ThemePlan/Themes/Works/PlanWorkAdditor.g.i.cs
@@ -54,8 +54,7 @@ namespace Wisdom.Controls.Tables.ThemePlan.Themes.Works {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Wisdom;V1.0.0.0;component/controls/tables/themeplan/themes/works/planworkadditor" +
-                    ".xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Wisdom;component/controls/tables/themeplan/themes/works/planworkadditor.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\..\..\..\..\Controls\Tables\ThemePlan\Themes\Works\PlanWorkAdditor.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
