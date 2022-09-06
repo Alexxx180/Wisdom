801903926c3d03c865ec1a0597346873a6957cbe
Tatarintsev Aleksandr <67857746+Alexxx180@users.noreply.github.com>
2022-09-06 14:01:44 +0300

    Refactoring: Level -> Stage

---
+++ Wisdom/Controls/Tables/Levels/Stage.xaml.cs
@@ -0,0 +1,21 @@
+using System.Windows;
+using System.Windows.Controls;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Wisdom.Customing;
+using Wisdom.Controls.Tables.Competetions;
+using ControlMaterials.Tables;
+
+namespace Wisdom.Controls.Tables.Levels
+{
+    /// <summary>
+    /// Education level editor
+    /// </summary>
+    public partial class Stage : OldItem<Level>, INotifyPropertyChanged
+    {
+        public Stage()
+        {
+            InitializeComponent();
+        }
+    }
+}
