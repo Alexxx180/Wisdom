fa7fab465141393b6776f47bbf197938c455abfb
Tatarintsev Aleksandr <67857746+Alexxx180@users.noreply.github.com>
2022-09-06 16:45:04 +0300

    Refactoring: Source Type Additor

---
+++ Wisdom/Controls/Tables/Sources/SourceTypes/SourceTypeAdditor.xaml.cs
@@ -0,0 +1,58 @@
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Controls;
+using Wisdom.Customing;
+using ControlMaterials.Tables;
+using System.Windows.Input;
+using Wisdom.ViewModel.Commands;
+using ControlMaterials.Total;
+
+namespace Wisdom.Controls.Tables.Sources.SourceTypes
+{
+    /// <summary>
+    /// Special component to add new source type group
+    /// </summary>
+    public partial class SourceTypeAdditor : UserControl, INotifyPropertyChanged
+    {
+        #region Dependency Properties
+        public static readonly DependencyProperty
+            VariantsProperty = DependencyProperty.Register(nameof(Variants),
+                typeof(ObservableCollection<string>), typeof(SourceType));
+        
+        public static readonly DependencyProperty
+            DataProperty = DependencyProperty.Register(nameof(Data),
+                typeof(NameNode<IndexedLabel>), typeof(SourceType));
+
+        public static readonly DependencyProperty
+            RemoveProperty = DependencyProperty.Register(nameof(Remove),
+                typeof(ICommand), typeof(SourceType));
+
+        public ICommand Remove
+        {
+            get => GetValue(RemoveProperty) as ICommand;
+            set => SetValue(RemoveProperty, value);
+        }
+        
+        public NameNode<IndexedLabel> Data
+        {
+            get => GetValue(DataProperty) as Source;
+            set => SetValue(DataProperty, value);
+        }
+        
+        public ObservableCollection<string> Variants
+        {
+            get => GetValue(VariantsProperty) as ObservableCollection<string>;
+            set => SetValue(VariantsProperty, value);
+        }
+        #endregion
+
+        public SourceTypeElementAdditor()
+        {
+            InitializeComponent();
+            Data = new NameNode<IndexedLabel>();
+        }
+    }
+}
