ca018a91952e134187b6f379eaf9d4c0d845f507
Tatarintsev Aleksandr <67857746+Alexxx180@users.noreply.github.com>
2022-09-07 14:02:41 +0300

    Plan Theme Additor: new item template

--- Wisdom/Controls/Tables/ThemePlan/Themes/PlanThemeAdditor.xaml.cs
+++ Wisdom/Controls/Tables/ThemePlan/Themes/PlanThemeAdditor.xaml.cs
@@ -2,110 +2,20 @@
 using System.Runtime.CompilerServices;
 using System.Windows;
 using System.Windows.Controls;
+using ControlMaterials.Tables.ThemePlan;
+using Wisdom.Control
 
 namespace Wisdom.Controls.Tables.ThemePlan.Themes
 {
     /// <summary>
     /// Special component to add new theme to topic
     /// </summary>
-    public partial class PlanThemeAdditor : UserControl, INotifyPropertyChanged, IAutoIndexing
+    public partial class PlanThemeAdditor : NewItem<Theme>
     {
-        public static readonly DependencyProperty
-            TopicProperty = DependencyProperty.Register(nameof(Topic),
-                typeof(PlanTopic), typeof(PlanThemeAdditor));
-
-        #region IAutoIndexing Members
-        private uint _no;
-        public uint No
-        {
-            get => _no;
-            set
-            {
-                _no = value;
-                OnPropertyChanged();
-            }
-        }
-
-        public void Index(uint no)
-        {
-            No = no;
-        }
-        #endregion
-
-        #region PlanThemeAdditor Members
-        public PlanTopic Topic
-        {
-            get => GetValue(TopicProperty) as PlanTopic;
-            set => SetValue(TopicProperty, value);
-        }
-
-        private string _themeName;
-        public string ThemeName
-        {
-            get => _themeName;
-            set
-            {
-                _themeName = value;
-                OnPropertyChanged();
-            }
-        }
-
-        private string _themeHours;
-        public string ThemeHours
-        {
-            get => _themeHours;
-            set
-            {
-                _themeHours = value;
-                OnPropertyChanged();
-            }
-        }
-
-        private string _themeCompetetions;
-        public string ThemeCompetetions
-        {
-            get => _themeCompetetions;
-            set
-            {
-                _themeCompetetions = value;
-                OnPropertyChanged();
-            }
-        }
-
-        private string _themeLevel;
-        public string ThemeLevel
-        {
-            get => _themeLevel;
-            set
-            {
-                _themeLevel = value;
-                OnPropertyChanged();
-            }
-        }
-        #endregion
-
         public PlanThemeAdditor()
         {
             InitializeComponent();
-            Index(1);
+            Data = new Theme();
         }
-
-        #region INotifyPropertyChanged Members
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        /// <summary>
-        /// Raises this object's PropertyChanged event.
-        /// </summary>
-        /// <param name="propertyName">The property that has a new value.</param>
-        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            PropertyChangedEventHandler handler = PropertyChanged;
-            if (handler != null)
-            {
-                PropertyChangedEventArgs e = new PropertyChangedEventArgs(propertyName);
-                handler(this, e);
-            }
-        }
-        #endregion
     }
 }
