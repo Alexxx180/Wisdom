d88cf7d9d4a929dde8eb3c051e6c526d1207c02b
Tatarintsev Aleksandr <67857746+Alexxx180@users.noreply.github.com>
2022-09-07 12:40:29 +0300

    Plan Theme: old item template

--- Wisdom/Controls/Tables/ThemePlan/Themes/PlanTheme.xaml.cs
+++ Wisdom/Controls/Tables/ThemePlan/Themes/PlanTheme.xaml.cs
@@ -4,6 +4,7 @@ using System.Runtime.CompilerServices;
 using System.Windows;
 using System.Windows.Controls;
 using Wisdom.Controls.Tables.ThemePlan.Themes.Works;
+using Wisdom.Controls.Templates;
 using Wisdom.Customing;
 using ControlMaterials.Tables.ThemePlan;
 using System.Windows.Input;
@@ -13,48 +14,20 @@ namespace Wisdom.Controls.Tables.ThemePlan.Themes
     /// <summary>
     /// Theme of theme plan's topic
     /// </summary>
-    public partial class PlanTheme : UserControl, INotifyPropertyChanged, IExtendableItems, IWrapFields
+    public partial class PlanTheme : OldItem<Theme>, IExtendableItems, IWrapFields
     {
-        #region Dependency Properties
-        public static readonly DependencyProperty
-            DataProperty = DependencyProperty.Register(nameof(Data),
-                typeof(Theme), typeof(PlanTheme));
-
-        public static readonly DependencyProperty
-            RemoveProperty = DependencyProperty.Register(nameof(Remove),
-                typeof(ICommand), typeof(PlanTheme));
-                
+        #region Dependency Properties              
         public static readonly DependencyProperty
             RemoveWorkProperty = DependencyProperty.Register(nameof(RemoveWork),
                 typeof(ICommand), typeof(PlanTheme));
-
-        public ICommand Remove
-        {
-            get => GetValue(RemoveProperty) as ICommand;
-            set => SetValue(RemoveProperty, value);
-        }
         
         public ICommand RemoveWork
         {
             get => GetValue(RemoveWorkProperty) as ICommand;
             set => SetValue(RemoveWorkProperty, value);
         }
+        #endregion
         
-        public Theme Data
-        {
-            get => GetValue(DataProperty) as Theme;
-            set => SetValue(DataProperty, value);
-        }
-        #endregion
-
-        #region Theme Members
-        public void RefreshHours()
-        {
-            OnPropertyChanged(nameof(Works));
-            //Topic.RefreshHours();
-        }
-        #endregion
-
         #region IExtendableItems Members
         private bool _extended;
         public bool Extended
@@ -97,23 +70,5 @@ namespace Wisdom.Controls.Tables.ThemePlan.Themes
             Extended = true;
             IsWrap = false;
         }
-
-        #region INotifyPropertyChanged Members
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        /// <summary>
-        /// Raises this object's PropertyChanged event.
-        /// </summary>
-        /// <param name="propertyName">The property that has a new value.</param>
-        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            PropertyChangedEventHandler handler = PropertyChanged;
-            if (handler != null)
-            {
-                PropertyChangedEventArgs e = new PropertyChangedEventArgs(propertyName);
-                handler(this, e);
-            }
-        }
-        #endregion
     }
 }
