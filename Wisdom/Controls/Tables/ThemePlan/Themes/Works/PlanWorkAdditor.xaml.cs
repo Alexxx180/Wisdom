c7eb28403f07ff8bdfbdb3ffb57ab6c22c1bb704
Tatarintsev Aleksandr <67857746+Alexxx180@users.noreply.github.com>
2022-09-07 15:46:00 +0300

    Plan Work Additor: new item template

--- Wisdom/Controls/Tables/ThemePlan/Themes/Works/PlanWorkAdditor.xaml.cs
+++ Wisdom/Controls/Tables/ThemePlan/Themes/Works/PlanWorkAdditor.xaml.cs
@@ -3,6 +3,7 @@ using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Windows;
 using System.Windows.Controls;
+using Wisdom.Controls.Templates;
 using ControlMaterials.Tables.ThemePlan;
 
 namespace Wisdom.Controls.Tables.ThemePlan.Themes.Works
@@ -10,76 +11,12 @@ namespace Wisdom.Controls.Tables.ThemePlan.Themes.Works
     /// <summary>
     /// Special component to add new work to theme
     /// </summary>
-    public partial class PlanWorkAdditor : UserControl, INotifyPropertyChanged
+    public partial class PlanWorkAdditor : NewItem<Work>, INotifyPropertyChanged
     {
-        public static readonly DependencyProperty
-            ThemeProperty = DependencyProperty.Register(nameof(Theme),
-                typeof(PlanTheme), typeof(PlanWorkAdditor));
-
-        public static readonly DependencyProperty
-            TypesProperty = DependencyProperty.Register(nameof(Types),
-                typeof(ObservableCollection<string>),
-                typeof(PlanWorkAdditor));
-
-        #region WorkAdditor Members
-        public static ObservableCollection<string> AvailableTypes { get; set; }
-
-        public ObservableCollection<string> Types
-        {
-            get => GetValue(TypesProperty) as ObservableCollection<string>;
-            set => SetValue(TypesProperty, value);
-        }
-
-        public PlanTheme Theme
-        {
-            get => GetValue(ThemeProperty) as PlanTheme;
-            set => SetValue(ThemeProperty, value);
-        }
-
-        public string _workType;
-        public string WorkType
-        {
-            get => _workType;
-            set
-            {
-                _workType = value;
-                OnPropertyChanged();
-            }
-        }
-
-        public bool _isGroup;
-        public bool IsGroup
-        {
-            get => _isGroup;
-            set
-            {
-                _isGroup = value;
-                OnPropertyChanged();
-            }
-        }
-        #endregion
-
         public PlanWorkAdditor()
         {
             InitializeComponent();
+            Data = new Work();
         }
-
-        #region INotifyPropertyChanged Members
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        /// <summary>
-        /// Raises this object's PropertyChanged event.
-        /// </summary>
-        /// <param name="propertyName">The property that has a new value.</param>
-        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            PropertyChangedEventHandler handler = PropertyChanged;
-            if (handler != null)
-            {
-                PropertyChangedEventArgs e = new PropertyChangedEventArgs(propertyName);
-                handler(this, e);
-            }
-        }
-        #endregion
     }
 }
