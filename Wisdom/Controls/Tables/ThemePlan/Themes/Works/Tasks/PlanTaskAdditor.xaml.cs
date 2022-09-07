978270e5e636ecabb2c18a8ee711a7325e279587
Tatarintsev Aleksandr <67857746+Alexxx180@users.noreply.github.com>
2022-09-07 16:16:55 +0300

    Plan Task Additor: new item template

--- Wisdom/Controls/Tables/ThemePlan/Themes/Works/Tasks/PlanTaskAdditor.xaml.cs
+++ Wisdom/Controls/Tables/ThemePlan/Themes/Works/Tasks/PlanTaskAdditor.xaml.cs
@@ -2,91 +2,20 @@
 using System.Runtime.CompilerServices;
 using System.Windows;
 using System.Windows.Controls;
+using ControlMaterials.Tables.ThemePlan;
+using Wisdom.Controls.Templates;
 
 namespace Wisdom.Controls.Tables.ThemePlan.Themes.Works.Tasks
 {
     /// <summary>
     /// Special component to add new task to work
     /// </summary>
-    public partial class PlanTaskAdditor : UserControl, INotifyPropertyChanged, IAutoIndexing
+    public partial class PlanTaskAdditor : NewItem<Task>, INotifyPropertyChanged, IAutoIndexing
     {
-        public static readonly DependencyProperty
-            WorkProperty = DependencyProperty.Register(nameof(Work),
-                typeof(PlanWork), typeof(PlanTaskAdditor));
-
-        #region IAutoIndexing Members
-        private uint _no;
-        public uint No
-        {
-            get => _no;
-            set
-            {
-                _no = value;
-                OnPropertyChanged();
-                OnPropertyChanged(nameof(TaskHeader));
-            }
-        }
-
-        public void Index(uint no)
-        {
-            No = no;
-        }
-        #endregion
-
-        #region PlanTaskAdditor Members
-        public PlanWork Work
-        {
-            get => GetValue(WorkProperty) as PlanWork;
-            set => SetValue(WorkProperty, value);
-        }
-
-        public string TaskHeader => $"{No}.";
-
-        public string _taskName;
-        public string TaskName
-        {
-            get => _taskName;
-            set
-            {
-                _taskName = value;
-                OnPropertyChanged();
-            }
-        }
-
-        public string _taskHours;
-        public string TaskHours
-        {
-            get => _taskHours;
-            set
-            {
-                _taskHours = value;
-                OnPropertyChanged();
-            }
-        }
-        #endregion
-
         public PlanTaskAdditor()
         {
             InitializeComponent();
-            Index(1);
+            Data = new Task();
         }
-
-        #region INotifyPropertyChanged Members
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        /// <summary>
-        /// Raises this object's PropertyChanged event.
-        /// </summary>
-        /// <param name="propertyName">The property that has a new value.</param>
-        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            PropertyChangedEventHandler handler = PropertyChanged;
-            if (handler != null)
-            {
-                PropertyChangedEventArgs e = new PropertyChangedEventArgs(propertyName);
-                handler(this, e);
-            }
-        }
-        #endregion
     }
 }
