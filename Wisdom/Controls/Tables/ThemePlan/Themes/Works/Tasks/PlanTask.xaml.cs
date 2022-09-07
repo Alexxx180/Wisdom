9379e56418415cb3ebd00348b63b020463fcd9fb
Tatarintsev Aleksandr <67857746+Alexxx180@users.noreply.github.com>
2022-09-07 15:55:43 +0300

    Plan Task: old item template

--- Wisdom/Controls/Tables/ThemePlan/Themes/Works/Tasks/PlanTask.xaml.cs
+++ Wisdom/Controls/Tables/ThemePlan/Themes/Works/Tasks/PlanTask.xaml.cs
@@ -5,36 +5,15 @@ using System.Runtime.CompilerServices;
 using ControlMaterials.Tables.ThemePlan;
 using ControlMaterials.Tables;
 using System.Windows.Input;
+using Wisdom.Controls.Templates;
 
 namespace Wisdom.Controls.Tables.ThemePlan.Themes.Works.Tasks
 {
     /// <summary>
     /// Task of theme's work
     /// </summary>
-    public partial class PlanTask : UserControl, INotifyPropertyChanged, IWrapFields
+    public partial class PlanTask : OldItem<IndexedHour>, INotifyPropertyChanged, IWrapFields
     {
-        #region Dependency Properties
-        public static readonly DependencyProperty
-            DataProperty = DependencyProperty.Register(nameof(Data),
-                typeof(Theme), typeof(PlanTask));
-
-        public static readonly DependencyProperty
-            RemoveProperty = DependencyProperty.Register(nameof(Remove),
-                typeof(ICommand), typeof(PlanTask));
-
-        public ICommand Remove
-        {
-            get => GetValue(RemoveProperty) as ICommand;
-            set => SetValue(RemoveProperty, value);
-        }
-               
-        public IndexedHour Data
-        {
-            get => GetValue(DataProperty) as IndexedHour;
-            set => SetValue(DataProperty, value);
-        }
-        #endregion
-
         #region IWrapFields Members
         private bool _isWrap;
         public bool IsWrap
@@ -58,23 +37,5 @@ namespace Wisdom.Controls.Tables.ThemePlan.Themes.Works.Tasks
             InitializeComponent();
             IsWrap = false;
         }
-        
-        #region INotifyPropertyChanged Members
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        /// <summary>
-        /// Raises this object's PropertyChanged event.
-        /// </summary>
-        /// <param name="propertyName">The property that has a new value.</param>
-        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            PropertyChangedEventHandler handler = PropertyChanged;
-            if (handler != null)
-            {
-                PropertyChangedEventArgs e = new PropertyChangedEventArgs(propertyName);
-                handler(this, e);
-            }
-        }
-        #endregion
     }
 }
