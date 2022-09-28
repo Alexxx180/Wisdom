8648c0eb86f6fea625c54925fd98ceb934a55b3a
Alexxx180 <alexx120702@gmail.com>
2022-09-28 19:49:06 +0300

    Migrated Theme Plan to data/control templates

---
+++ Wisdom/Controls/Tables/Additor.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Wisdom.Controls.Tables
+{
+    /// <summary>
+    /// Логика взаимодействия для Additor.xaml
+    /// </summary>
+    public partial class Additor : UserControl
+    {
+        public static readonly DependencyProperty
+            ItemProperty = DependencyProperty.Register(nameof(Item),
+                typeof(ControlTemplate), typeof(Additor));
+
+        public ControlTemplate Item
+        {
+            get => GetValue(ItemProperty) as ControlTemplate;
+            set => SetValue(ItemProperty, value);
+        }
+
+        public Additor()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Wisdom/Controls/Tables/Remover.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Wisdom.Controls.Tables
+{
+    /// <summary>
+    /// Логика взаимодействия для Remover.xaml
+    /// </summary>
+    public partial class Remover : UserControl
+    {
+        public static readonly DependencyProperty
+            ItemProperty = DependencyProperty.Register(nameof(Item),
+                typeof(ControlTemplate), typeof(Remover));
+
+        public ControlTemplate Item
+        {
+            get => GetValue(ItemProperty) as ControlTemplate;
+            set => SetValue(ItemProperty, value);
+        }
+
+        public Remover()
+        {
+            InitializeComponent();
+        }
+    }
+}
